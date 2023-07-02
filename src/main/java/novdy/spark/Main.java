1c146eb6bfb3265e677a09f8799c4fedc552e5dc
peter <peter3hayes@gmail.com>
2023-07-02 18:53:40 -0500

    added styling; modified layout

--- src/main/java/novdy/spark/Main.java
+++ src/main/java/novdy/spark/Main.java
@@ -1,75 +1,7 @@
 package novdy.spark;
 
-import javafx.application.Application;
-import javafx.application.Platform;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.*;
-import javafx.scene.paint.Color;
-import javafx.stage.Stage;
-import javafx.stage.StageStyle;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Main extends Application {
-    double mouseAnchorX;
-    double mouseAnchorY;
-
+public class Main {
     public static void main(String[] args) {
-        launch(args);
-    }
-
-    @Override
-    public void start(Stage stage) {
-        Path resourcePath = Paths.get("target/classes");
-
-        stage.initStyle(StageStyle.TRANSPARENT);
-
-        GridPane root = new TransparentPane(stage);
-        FlowPane content = new FlowPane();
-        VBox buttons = new VBox();
-        buttons.setPrefWidth(30);
-        buttons.setId("buttons");
-
-        Button closeButton = new Button("X");
-        Button settingsButton = new Button("S");
-        Button cropButton = new Button("C");
-        closeButton.setMaxWidth(Double.MAX_VALUE);;
-        settingsButton.setMaxWidth(Double.MAX_VALUE);
-        cropButton.setMaxWidth(Double.MAX_VALUE);
-        cropButton.setPrefHeight(10000);
-
-        closeButton.setOnAction(actionEvent -> {
-            Platform.exit();
-        });
-
-//        for(int i = 0; i < 40; i++){
-//            content.getChildren().add(new Button("テスト"));
-//        }
-
-        buttons.getChildren().add(closeButton);
-        buttons.getChildren().add(settingsButton);
-        buttons.getChildren().add(cropButton);
-
-        root.add(content, 0,0,1,1);
-        root.add(buttons,1,0,1,1);
-//        root.setGridLinesVisible(true);
-
-        GridPane.setHgrow(content, Priority.SOMETIMES);
-        GridPane.setVgrow(buttons, Priority.ALWAYS);
-
-        Scene scene = new Scene(root, 960, 270);
-
-        scene.getStylesheets().add(Paths.get(resourcePath.toString(), "general.css").toUri().toString());
-        scene.setFill(Color.TRANSPARENT);
-
-        stage.setScene(scene);
-        stage.setTitle("Spark OCR");
-        stage.setAlwaysOnTop(true);
-        ResizeHelper.addResizeListener(stage);
-
-        stage.show();
+        App.main(args);
     }
 }
